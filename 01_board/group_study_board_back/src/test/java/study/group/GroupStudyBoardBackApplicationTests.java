1ba49946a8d1cfabf32fee5bed18e912fb281953
Donghyo-Son <143981349+DonghyoSon@users.noreply.github.com>
2023-11-28 12:52:04 +0900

    백 버전 문제 해결(테스트 필요)

---
+++ 01_board/group_study_board_back/src/test/java/study/group/GroupStudyBoardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package study.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupStudyBoardBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
