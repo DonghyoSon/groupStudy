47212a11316376adf275a1bdd8682c3b1dca8528
Donghyo-Son <143981349+DonghyoSon@users.noreply.github.com>
2023-12-13 18:57:56 +0900

    현재 페이지를 굵게 표시

--- 01_board/group_study_board_front/src/component/Pagination.js
+++ 01_board/group_study_board_front/src/component/Pagination.js
@@ -13,10 +13,24 @@ const Pagination = (props) => {
     setReqPage(changePage);
   };
 
+  //현재 페이지를 굵게 표시하는 함수 - React에서 HTML에 인라인 방식으로 style을 지정하는 방법
+  const fontBold = {
+    fontWeight: "bold",
+  };
+
   //페이지 숫자 버튼
   let pageNo = page.pageNo;
   for (let i = 0; i < page.pageNaviSize; i++) {
-    arr.push(<span onClick={changePageFunc}>{pageNo}</span>);
+    //현재 페이지를 구분하기 위해 <h5>태그 삽입
+    if (pageNo === Number(reqPage)) {
+      arr.push(
+        <span onClick={changePageFunc} style={fontBold}>
+          {pageNo}
+        </span>
+      );
+    } else {
+      arr.push(<span onClick={changePageFunc}>{pageNo}</span>);
+    }
 
     pageNo++;
 
